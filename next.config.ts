0102bf686f5db87dfa0710f23df9b1238865883d
Allen Chou <f1470891079@gmail.com>
2025-04-24 11:54:00 +0800

    Configure allowed external image domains for Next.js optimization
    - Add 'images.domains' to next.config.ts to whitelist trusted sources
    - Enable optimized image loading from Google, GitHub, Twitter, Facebook,
      Discord, Dicebear, Gravatar, and Imgur
    - Supports secure and efficient handling of user avatars and social media
      images via Next.js Image component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com",
+      "pbs.twimg.com",
+      "platform-lookaside.fbsbx.com",
+      "cdn.discordapp.com",
+      "avatars.dicebear.com",
+      "graph.facebook.com",
+      "s.gravatar.com",
+      "www.gravatar.com",
+      "imgur.com",
+    ],
+  },
 };
 
 export default nextConfig;
